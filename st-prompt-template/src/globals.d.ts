b692b8eaccec446d6f17ec3c9aef58507ee3b138
Albusken <16057870+albusking@user.noreply.gitee.com>
2026-03-19 00:20:09 +0800

    更新星数据库v1

--- st-prompt-template/src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// globals.d.ts
-/** @type {import('lodash').LoDashStatic} */
-declare const _: any;
-/** @type {import('jquery').JQueryStatic} */
-declare const $: any;
-/** @type {import('toastr').Toastr} */
-declare const toastr: any;
